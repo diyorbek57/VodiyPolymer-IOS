ce96fab7a05cbf525f3416f7b5fb6b9684515ad8
Diyorbek <hosmerdiyir57@gmail.com>
2022-09-30 15:55:32 +0500

    Initial Commit

---
+++ VodiyPolymer/VodiyPolymerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VodiyPolymerApp.swift
+//  VodiyPolymer
+//
+//  Created by Diyorbek Hamdamov on 30/09/22.
+//
+
+import SwiftUI
+
+@main
+struct VodiyPolymerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
